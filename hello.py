1df100ac0ef92c07be4030751e869da24eec583a
RAdamAnderson <42124094+RAdamAnderson@users.noreply.github.com>
2018-08-05 16:46:35 -0600

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,7 @@
+name = input("What's your name? ")
+if name!="Fart":
+    print ("Your name may be %s, but I am going to call you Fart." %(name))
+    name = "Fart"
+    print ("Hello, %s"%(name))
+else:
+    print("Hello, Fart.")
