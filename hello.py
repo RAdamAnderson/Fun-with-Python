3c4ae64e63ab533e091948b0301effca6120cced
RAdamAnderson <42124094+RAdamAnderson@users.noreply.github.com>
2018-08-05 21:34:05 -0600

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 def name():
     orig=input("Hello. Nice to meet you. What is your name? ")
     if len(orig)>0 and orig.isalpha() and orig!="Fart":
-        print("Hello, %s. %s is a dumb name. Your name is Fart now." % (orig))
+        print("Hello, %s. %s is a dumb name. Your name is Fart now." % (orig, orig))
         name()
     elif orig=="Fart":
         print("Hello, Fart.")
